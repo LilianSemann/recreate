d2e2ab3cbb8ae8003788183d312c98895157031c
Lilian Cristine Semann <Liliansemann2525@gmail.com>
2023-07-18 03:16:42 -0300

    INEEDTOGOTOBED

--- src/app/components/asidenav.tsx
+++ src/app/components/asidenav.tsx
@@ -1,32 +1,41 @@
 "use client"
 
-import Link from "next/link";
+import Image from 'next/image'
+import Link from "next/link"; 
+import { RxDashboard } from "react-icons/rx";
+import { PiSignInFill } from "react-icons/pi";
+import { BsRocketTakeoff } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { ImTable2 } from "react-icons/im";
 import { usePathname } from "next/navigation";
 
 export default function Sidenav() {
     const pathname = usePathname()
-
-    interface Item {
-        tittle: string,
-        link: string,
-    }
-
+    
     const pages = [
-        {tittle: "Home", link: "/"},
-        {tittle: "Dashboard", link: "/dashboard"},
-        {tittle: "Tables", link: "/tables"}
+        {icon: <AiOutlineHome/>, tittle: "Home", link: "/"},
+        {icon: <RxDashboard/>, tittle: "Dashboard", link: "/dashboard"},
+        {icon: <ImTable2/>, tittle: "Tables", link: "/tables"}
     ]
 
     const profile = [
-        {tittle: "Profile", link: "/profile"},
-        {tittle: "Sign in", link: "/signin"},
-        {tittle: "Sign up", link: "/signup"}
+        {icon: <CgProfile/>, tittle: "Profile", link: "/profile"},
+        {icon: <PiSignInFill/>, tittle: "Sign in", link: "/signin"},
+        {icon: <BsRocketTakeoff/>, tittle: "Sign up", link: "/signup"}
     ]
 
     const selectedStyle = {
         backgroundColor: "white",
         borderRadius: "10px",
+        fontWeight: "bold",
         boxShadow: "0 4px 8px 0 rgba(161, 161, 161, 0.2), 0 6px 20px 0 rgba(161, 161, 161, 0.19)"
+      }
+
+      const selectedIcon = {
+        backgroundColor: "pink",
+        color: "white",
+        boxShadow: "0 4px 8px 0 rgba(209, 209, 209, 0.2), 0 6px 20px 0 rgba(209, 209, 209, 0.19)"
       }
 
 
@@ -34,10 +43,11 @@ export default function Sidenav() {
     <section className="fixed hidden md:block md:w-60 h-[94%] left-3 rounded-xl shadow-lg overflow-auto bg-zinc-100">
         
         <div className="flex fixed z-50 bg-zinc-100 w-60 h-20 items-center justify-center">
-            cass
+            <Image src={"/amongus.png"} alt="Picture of the author" width={30} height={50}/>
+
         </div>
         <div className="flex justify-center">
-            <hr className="w-3/5 h-[2px] bg-gradient-to-r from-transparent via-gray-200 to-transparent mt-20"/>
+            <hr className="w-3/5 h-[1px] bg-gradient-to-r from-transparent via-zinc-300 to-transparent mt-20"/>
         </div>
         
         <div className="flex flex-col p-5">
@@ -46,8 +56,9 @@ export default function Sidenav() {
                     style={i.link == pathname ? selectedStyle : {}} 
                     key={i.link} 
                     href={i.link}
-                    className="p-3 hover:bg-zinc-200 rounded-[10px] transition duration-200"
-                    >
+                    className="p-3 hover:bg-zinc-200 rounded-[10px] transition duration-200 flex items-center gap-2"
+                >
+                    <span style={i.link == pathname ? selectedIcon : {}} className="bg-white p-2 w-9 h-9 rounded-lg flex justify-center items-center">{i.icon}</span>
                     {i.tittle}
                 </Link>
             ))}
@@ -57,8 +68,9 @@ export default function Sidenav() {
                     style={j.link == pathname ? selectedStyle : {}} 
                     key={j.link} 
                     href={j.link}
-                    className="p-3 hover:bg-zinc-200 rounded-[10px] transition duration-200"
+                    className="p-3 hover:bg-zinc-200 rounded-[10px] transition duration-200 flex items-center gap-2"
                     >
+                    <span style={j.link == pathname ? selectedIcon : {}} className="bg-white p-2 w-9 h-9 rounded-lg flex justify-center items-center">{j.icon}</span>
                     {j.tittle}
                 </Link>
             ))}
---
+++ src/app/tables/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Sidenav from "../components/asidenav"
+
+export default function Tables() {
+  return (
+    <section className="flex min-h-screen p-5 bg-zinc-100">
+      <Sidenav/>
+      <div className="flex md:w-9/12 md:absolute md:right-5 w-full justify-center bg-white rounded-xl p-5 shadow-lg">
+        World
+      </div>
+    </section>
+  )
+}
