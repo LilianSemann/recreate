9d866cb985270ed70247b0098a0a3bd1d5684b0c
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2024-04-05 16:59:49 -0300

    Update asidenav.tsx

--- src/app/components/asidenav.tsx
+++ src/app/components/asidenav.tsx
@@ -43,7 +43,7 @@ export default function Sidenav() {
     <section className="fixed hidden lg:block lg:w-60 h-[94%] left-3 rounded-xl shadow-lg overflow-auto bg-zinc-100">
         <Link href={"/"} className="flex fixed z-50 w-60 h-20 items-center justify-center gap-3 transform hover:scale-105 transition duration-300">
             <Image src={"/amongus.png"} alt="Picture of the author" width={30} height={50}/>
-            <p className='font-semibold'>Lils' Company</p>
+            <p className='font-semibold'>Lils Company</p>
         </Link>
         <div className="flex justify-center">
             <hr className="w-4/6 h-[0.099rem] bg-gradient-to-r from-transparent via-zinc-200 to-transparent mt-[5.5rem]"/>
